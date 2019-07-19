9cc542e6af79128c599f45faf8b692e3094814ec
lixuhui123 <2871726754@qq.com>
2019-07-19 23:56:05 +0800

    一些更新

--- queue/queue.cpp
+++ queue/queue.cpp
@@ -1,32 +1,39 @@
 #include "queue.h"
 void dequeueInit(Dequeue * qu)
 {
-	qu->_head = qu->_tail = qu->_data;
+	//初始化
+	qu->_head = qu->_tail = qu->_data;//头和尾都指向数组的首元素的首地址
 	qu->_size = 0;
 }
 void dequeueIEmpty(Dequeue * qu)
 {
+	//清空
 	qu->_head = qu->_tail;
 	qu->_size = 0;
 }
 int dequeuePush(Dequeue * qu, DataType X)
 {
+	//在循环队列中，尾巴上留一个不存数据的空间来检测队列是否已满
+    //环形队列当中出队从对头出入队从队尾入
 	*qu->_tail = X;
 	if (qu->_tail + 1 == qu->_head || (qu->_tail + 1 - qu->_data == QUEUENUM && qu->_head == qu->_tail))
 	{
+     //此种情况下队列已满不能再插入,第二个+1是元素有100个到99就会满所以减的时候要+1
 		return -1;
 	}
-	qu->_tail++;
+	qu->_tail++;//尾指针后移
 	if (qu->_tail - qu->_data == QUEUENUM)
 	{
+		//此时队列中的元素已经存到下标为100所以要重新初始化队列
 		qu->_tail = qu->_data;
 	}
 	qu->_size++;
 }
 void dequeuePop(Dequeue * qu)
 {
+	//出队列，头指针后移
 	qu->_head++;
-	if (qu->_head - qu->_data == QUEUENUM)
+	if (qu->_head - qu->_data == QUEUENUM-1)
 	{
 		qu->_head = qu->_data;
 	}
--- queue/queue.h
+++ queue/queue.h
@@ -1,15 +1,16 @@
 #pragma once
 #include<stdio.h>
 #include<stdlib.h>
+//用数组实现的循环队列
 #define  QUEUENUM 100
 typedef int DataType;
 typedef struct  
 {
-	DataType _data[QUEUENUM];
+	DataType _data[QUEUENUM];//这是一个数组
 	DataType *_head;
 	DataType *_tail;
 	size_t _size;
-}Dequeue;
+}Dequeue;//结构体类型
 void dequeueInit(Dequeue * qu);
  
 void dequeueIEmpty(Dequeue * qu);
